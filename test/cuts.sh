a5893bf3d70bcb3998ff3b33e52a0d31c4ef2a1b
Lars Kuhtz <lars@kadena.io>
2022-02-11 16:44:50 -0800

    update cuts script

--- test/cuts.sh
+++ test/cuts.sh
@@ -4,9 +4,9 @@
   echo "{"
   while read -r i ; do
       echo "\"$i\": "
-      curl -sk "https://$i:1789/chainweb/0.0/development/cut" || echo -n "{}"
+      curl -sk -timeout=1 "https://$i:1789/chainweb/0.0/development/cut" || echo -n "{}"
       [[ "$i" = "db-server-node" ]] || echo ","
   done < ./NODES.txt
   echo "}"
 } |
-jq
+jq -crM
