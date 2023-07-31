1bf900ad1bda73c0367f7e4d70ec7515ea671de2
Andy Tang <andy.tang@action.nl>
2023-07-31 08:17:36 +0200

    Update to the correct spv branch

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:kadena-io/devenv/devnet-setup";
-    chainweb-node.url = "github:kadena-io/chainweb-node/edmund/fast-devnet";
+    chainweb-node.url = "github:kadena-io/chainweb-node/edmund/l2-spv-poc";
     chainweb-data.url = "github:kadena-io/chainweb-data";
     chainweb-mining-client = {
       url = "github:kadena-io/chainweb-mining-client/enis/update-to-flakes-and-haskellNix";
