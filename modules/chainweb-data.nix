7649c34e2e3598d62941553251d7585de5d207d0
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2023-08-03 15:23:06 +0200

    Add chainweb-data

--- modules/chainweb-data.nix
+++ modules/chainweb-data.nix
@@ -1,8 +1,25 @@
-{ pkgs, ...}:
+{ pkgs, config, ...}:
 let
+  absolutePgData = "$(${pkgs.coreutils}/bin/realpath ${config.env.PGDATA})";
+  start-chainweb-data = pkgs.writeShellScript "start-chainweb-data" ''
+    ${pkgs.chainweb-data}/bin/chainweb-data \
+      --dbstring "postgresql:///$USER?host=${absolutePgData}" \
+      --service-host localhost --p2p-host localhost --p2p-port 1789 \
+      --migrate server --port 1849 --serve-swagger-ui
+  '';
 in
 {
   config = {
+    packages = [ pkgs.chainweb-data ];
+
+    processes.chainweb-data = {
+      exec = "${start-chainweb-data}";
+      process-compose.depends_on = {
+        chainweb-node.condition = "process_healthy";
+        postgres.condition = "process_healthy";
+      };
+    };
+
     services.postgres.enable = true;
   };
 }
