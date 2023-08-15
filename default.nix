ba1033343bbd06f77563a8abf0a63c7d62fba9ae
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2023-08-15 16:54:46 +0200

    Add a default.nix exposing the flake

---
+++ default.nix
@@ -0,0 +1,14 @@
+let
+  defaultNix = (import (
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/12c64ca55c1014cdc1b16ed5a804aa8576601ff2.tar.gz";
+      sha256 = "0jm6nzb83wa6ai17ly9fzpqc40wg1viib8klq8lby54agpl213w5"; }
+  ) {
+    src =  ./.;
+  }).defaultNix;
+in defaultNix // rec {
+  configurations = defaultNix.configurations.${builtins.currentSystem};
+  mkFlake = module: defaultNix.lib.${builtins.currentSystem}.mkFlake [module];
+  mkContainer = module: (mkFlake module).packages.container;
+  mkRunner = module: (mkFlake module).packages.default;
+}
