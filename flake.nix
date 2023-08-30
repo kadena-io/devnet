9aa884bc2c2aab0a05d7ff3df2b40b3fc1cf9618
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2023-08-30 23:25:53 +0200

    Update pact and chainweb-node inputs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     devenv.url = "github:cachix/devenv";
-    chainweb-node.url = "github:kadena-io/chainweb-node/edmund/fast-devnet";
+    chainweb-node.url = "github:kadena-io/chainweb-node";
     chainweb-node-l2.url = "github:kadena-io/chainweb-node/edmund/l2-spv-poc";
     chainweb-data = {
       url = "github:kadena-io/chainweb-data/enis/extra-migrations-folder";
@@ -15,7 +15,11 @@
       inputs.haskellNix.follows = "chainweb-node/haskellNix";
       inputs.nixpkgs.follows = "chainweb-node/nixpkgs";
     };
-    pact.url = "github:kadena-io/pact";
+    pact = {
+      url = "github:kadena-io/pact";
+      inputs.haskellNix.follows = "chainweb-node/haskellNix";
+      inputs.nixpkgs.follows = "chainweb-node/nixpkgs";
+    };
     nix-exe-bundle = { url = "github:3noch/nix-bundle-exe"; flake = false; };
   };
 
