6f78c1afbdc72dc8b8ace64bd8b7b330d3c331e2
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2023-08-31 12:16:03 +0200

    Document process-compose management API port

--- modules/process-compose.nix
+++ modules/process-compose.nix
@@ -1,6 +1,9 @@
-{...}:
+{lib,...}:
+with lib;
 {
   config = {
     process.implementation = "process-compose";
+    sites.landing-page.container-api.ports = mkAfter
+      "- `9999`: Process Compose management API";
   };
 }
