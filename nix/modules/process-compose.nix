0b438dfd27fc0df782fd35beac58fafde0a0d950
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2024-02-17 08:28:56 +0100

    Separate nixpkgs-unstable
    To keep most dependencies on the stable branch, while picking some from nixpkgs-unstable

--- nix/modules/process-compose.nix
+++ nix/modules/process-compose.nix
@@ -1,19 +1,20 @@
 {pkgs, lib, config, ...}:
 with lib;
 let
+  process-compose = config.process-managers.process-compose.package;
   pc-port = toString config.process-managers.process-compose.settings.port;
 in
 {
   imports = [
     (lib.mkIf config.services.ttyd.enable {
       services.ttyd.commands.process-compose =
-        "${pkgs.process-compose}/bin/process-compose attach -p ${pc-port}";
+        "${process-compose}/bin/process-compose attach -p ${pc-port}";
       sites.landing-page.main-links =
         "- [Process Manager](/ttyd/process-compose/)";
     })
   ];
   config = {
-    packages = [ pkgs.process-compose ];
+    packages = [ process-compose ];
 
     process.implementation = "process-compose";
     sites.landing-page.container-api.ports = mkAfter
