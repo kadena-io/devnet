1e810b90f1673875891d546606a4641159cb95a8
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2024-02-19 14:10:40 +0100

    Expose and document the `attach` script

--- nix/modules/process-compose.nix
+++ nix/modules/process-compose.nix
@@ -3,20 +3,25 @@ with lib;
 let
   process-compose = config.process-managers.process-compose.package;
   pc-port = toString config.process-managers.process-compose.settings.port;
+  attach = pkgs.writeShellScriptBin "attach" ''
+    exec ${process-compose}/bin/process-compose attach -p ${pc-port}
+  '';
 in
 {
   imports = [
     (lib.mkIf config.services.ttyd.enable {
       services.ttyd.commands.process-compose =
-        "${process-compose}/bin/process-compose attach -p ${pc-port}";
+        "${attach}/bin/attach";
       sites.landing-page.main-links =
         "- [Process Manager](/ttyd/process-compose/)";
     })
   ];
   config = {
-    packages = [ process-compose ];
+    packages = [ process-compose attach ];
 
     process.implementation = "process-compose";
+    sites.landing-page.commands.process-compose.markdown =
+      "* `attach`: Attach to the Process Compose management interface";
     sites.landing-page.container-api.ports = mkAfter
       "- `${pc-port}`: Process Compose management API";
   };
