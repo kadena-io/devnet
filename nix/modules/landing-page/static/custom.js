0530103011c19e6ca69a4daaf49453b586c20835
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2024-03-20 15:35:14 +0100

    Simplify openDetailsIfHashMatches

--- nix/modules/landing-page/static/custom.js
+++ nix/modules/landing-page/static/custom.js
@@ -8,20 +8,18 @@ window.addEventListener('hashchange', (event) => {
 
 function openDetailsIfHashMatches() {
     const hash = window.location.hash;
-    if (hash) {
-        const targetElement = document.querySelector(hash);
-        if (targetElement) {
-            let parent = targetElement.parentElement;
-            while (parent) {
-                if (parent.tagName.toLowerCase() === 'details' && !parent.open) {
-                    parent.open = true;
-                    break;
-                }
-                parent = parent.parentElement;
-            }
-            requestAnimationFrame(() => {
-                targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
-            });
-        }
+    if (!hash) return;
+    const targetElement = document.querySelector(hash);
+    if (!targetElement) return;
+    let parent = targetElement.parentElement;
+    while (parent) {
+      if (parent.tagName.toLowerCase() === 'details' && !parent.open) {
+          parent.open = true;
+          break;
+      }
+      parent = parent.parentElement;
     }
+    requestAnimationFrame(() => {
+        targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
+    });
 }
