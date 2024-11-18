3a2e870d4272d9d263a8e07b52dd54b355e44294
Louis Page <lapsmores@gmail.com>
2024-11-18 15:11:15 -0600

    adding update for marcin's fork

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     # the pact package from its cabal.project, but a non-"empty" flake overrides it.
     pact-override.follows = "chainweb-node/empty";
     chainweb-node = {
-      url = "github:kadena-io/chainweb-node";
+      url = "github:F1bonacc1/process-compose";
       inputs.pact.follows = "pact-override";
     };
 
