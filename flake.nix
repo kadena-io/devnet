95fcbf92ea0a6a11d9114097b09668ea7ceacc0f
chessai <chessai1996@gmail.com>
2025-02-28 11:46:50 -0800

    bump chainweb

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     # the pact package from its cabal.project, but a non-"empty" flake overrides it.
     pact-override.follows = "chainweb-node/empty";
     chainweb-node = {
-      url = "github:kadena-io/chainweb-node/b387d1e888c1b66e39dd580372867042e5bdf9b9";
+      url = "github:kadena-io/chainweb-node";
       inputs.pact.follows = "pact-override";
     };
 
